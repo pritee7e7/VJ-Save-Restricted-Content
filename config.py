f0fe5332c8126eacadb150cd6f07399f053476dc
pritee7e7 <ahirwalp62@gmail.com>
2025-04-07 15:42:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,17 +4,17 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 # Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", "29232353"))
+API_ID = int(os.environ.get("API_ID", "28473509"))
 
 # Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "6868788228291767c90e4346eea03f36")
+API_HASH = os.environ.get("API_HASH", "f56218a21931d5f4ddcf0f0354256816")
 
 # Your Owner / Admin Id For Broadcast 
-ADMINS = int(os.environ.get("ADMINS", "6650855788"))
+ADMINS = int(os.environ.get("ADMINS", "5698467921"))
 
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://veerahirwal51:tqQKaR2ZSDQhtIYm@cluster0.dimp6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://luckyraj2297:nca4hJQmU4a9EN2L@cluster0.fv2xk7e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = os.environ.get("DB_NAME", "luckybot")
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
